6297f549bb6d82aa43923b93adb1cd308706a42e
MlleCanary <soccergirl2354@gmail.com>
2021-09-20 16:42:17 -0400

    rules.mkviaupdateRedox

--- keyboards/redox/rules.mk
+++ keyboards/redox/rules.mk
@@ -28,6 +28,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+VIA_ENABLE = yes
 
 SPLIT_KEYBOARD = yes
 
