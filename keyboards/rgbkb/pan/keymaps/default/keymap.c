fcc0da4ed097f276cd6406c6d31c40ea02da7759
MlleCanary <soccergirl2354@gmail.com>
2021-09-24 20:57:32 -0400

    panrev

--- keyboards/rgbkb/pan/keymaps/default/keymap.c
+++ keyboards/rgbkb/pan/keymaps/default/keymap.c
@@ -35,11 +35,11 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_all(
         KC_NO,                                                                                                      KC_NO,
-        GRAVE_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_DEL,
+        GRAVE_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_BSPC,
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         FN_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT,   KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-        KC_LCTL,   KC_LGUI, KC_LALT, RGB_TOG, MO(_ADJ),KC_SPC, KC_SPC, MO(_FN), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
+        KC_LCTL,   KC_LGUI, KC_LALT, RGB_TOG, MO(_ADJ),TO(_ADJ), KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_SLSH,   KC_DEL
     ),
 
     [_COLEMAK] = LAYOUT_all(
