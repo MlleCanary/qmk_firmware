0f0b6963c82e2dd197ee7644ff8f76b643a50fe4
MlleCanary <soccergirl2354@gmail.com>
2021-11-16 16:46:55 -0500

    LevinsonCreated
    Keymap edits and new map created

---
+++ keyboards/keebio/levinson/keymaps/Mlle/keymap.c
@@ -0,0 +1,84 @@
+#include QMK_KEYBOARD_H
+
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BASE 0
+#define _ONE 1
+
+enum custom_keycodes {
+  BASE = SAFE_RANGE,
+  ONE
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Base
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Ctrl| LAlt | Copy  | ~`  |\| |Layer |Space |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_BASE] = LAYOUT_ortho_4x12( \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_CTL,  KC_LALT, KC_GRV, KC_BSLS, LOWER,   TG(1),  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_LGUI,   KC_DEL \
+),
+
+/* One
+ * ,-----------------------------------------------------------------------------------.
+ * | `~  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | -_ |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | __  |  __  | __ |   Up  | ___ | PgUp | Mute  |Vol+  | ___|   |\  | =+  |  Bksp   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|RGB  |Left  | Down  | Rght  | PgDwn|   K  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Calc| Ctrl | ___ | ___ |___ |Layer |Space |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ONE] = LAYOUT_ortho_4x12( \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
+  _______,  KC_A,    KC_R,    KC_UP,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
+  KC_LSFT, RGB_TOG,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_CALC,  KC_LCTL, ___, ___, ___,   BASE,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+
+};
+
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case BASE:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_qwerty);
+        #endif
+        persistent_default_layer_set(1UL<<_BASE);
+      }
+      return false;
+      break;
+    case ONE:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_colemak);
+        #endif
+        persistent_default_layer_set(1UL<<_ONE);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
--- keyboards/rgbkb/pan/keymaps/rev2/keymap.c
+++ keyboards/rgbkb/pan/keymaps/rev2/keymap.c
@@ -19,58 +19,64 @@
 // entirely and just use numbers.
 #include QMK_KEYBOARD_H
 
-enum layers {
+#define _BASE 
+#define _RAISE 
+#define _ADJUST  
+#define _ONE 
+
+enum custom_keycodes  {
     _BASE,
-    _ONE,
-    _FN,
-    _ADJ
+    _RAISE,
+    _ADJUST,
+    _ONE 
 };
 
-#define FN_ESC   LT(_FN, KC_ESC)
-#define FN_CAPS  LT(_FN, KC_CAPS)
-#define BASE   DF(_BASE)
-#define ONE  DF(_ONE)
+
+#define FN_ESC   LT(_RAISE, KC_ESC)
+#define FN_CAPS  LT(_RAISE, KC_CAPS)
+#define QWERTY   DF(_BASE)
+#define COLEMAK  DF(_ADJUST)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_all(
-        KC_NO,                                                                                                      KC_MUTE,
+        KC_NO,                                                                                                      KC_NO,
         GRAVE_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_BSPC,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_GRV,    KC_A,     KC_S,   KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT,   KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_QUOT, _______,
-        KC_LCTL,   KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_LEFT, KC_QUOT, KC_QUOT,   KC_DEL
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, 
+        FN_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT,   KC_Z, KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM,    KC_DOT, KC_SLSH,  KC_SLSH, _______,
+        KC_LCTL,   KC_LGUI, KC_LALT, RGB_TOG, KC_SPC, TG(_ONE), KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_QUOT,   KC_DEL
+    ),
+
+    [_ADJUST] = LAYOUT_all(
+        KC_NO,                                                                                                      KC_NO,
+        GRAVE_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, _______,
+        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+        FN_CAPS,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, _______,
+        KC_LCTL,   KC_LGUI, KC_LALT, RGB_TOG, MO(_ONE),KC_SPC, KC_SPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
+    ),
+
+    [_RAISE] = LAYOUT_all(
+        KC_NO,                                                                                                      KC_NO,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______,
+        _______, KC_PGDN, KC_UP,   KC_PGUP, _______, _______, _______, KC_PGDN, KC_UP,   KC_PGUP, KC_PSCR, KC_HOME,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_END,
+        _______, _______, _______, _______, _______, _______, _______, KC_LBRC, _______, KC_RBRC, _______, _______, _______,
+        _______, _______, _______, RGB_MOD, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, _BASE, _BASE
     ),
 
     [_ONE] = LAYOUT_all(
         KC_NO,                                                                                                      KC_NO,
-        GRAVE_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_DEL,
-        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-        FN_CAPS,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_ENT,
-        KC_LCTL,   KC_LGUI, KC_LALT, RGB_TOG, TG(_ONE),KC_SPC, KC_SPC, MO(_FN), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
-    ),
-
-    [_FN] = LAYOUT_all(
-        KC_NO,                                                                                                      KC_NO,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_PSCR,
-        _______, KC_PGDN, KC_UP,   KC_PGUP, _______, _______, _______, KC_PGDN, KC_UP,   KC_PGUP, KC_PSCR, KC_HOME,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_END,
-        _______, _______, _______, _______, _______, _______, _______, KC_LBRC, _______, KC_RBRC, _______, _______, _______,
-        _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-
-    [_ADJ] = LAYOUT_all(
-        KC_NO,                                                                                                      KC_NO,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_PSCR,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______,
         _______, RGB_SAD, RGB_VAI, RGB_SAI, RESET,   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
         _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
         _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______, _______,
-        _______, _______, _______, RGB_MOD, TG(_BASE), _______, _______, KC_P0,   KC_PDOT, KC_NLCK, _BASE, _ONE
+        _______, _______, _______, RGB_MOD, TG(_BASE), TG(_BASE), TG(_BASE), KC_P0,   KC_PDOT, KC_NLCK, _BASE, _BASE
     )
 };
 
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 void oled_task_user(void) {
     // Host Keyboard Layer Status
     oled_write_P(PSTR("RGBKB Pan\n"), false);
@@ -78,16 +84,16 @@ void oled_task_user(void) {
     uint8_t layer = layer_state ? biton(layer_state) : biton32(default_layer_state);
     switch (layer) {
         case _BASE:
-            oled_write_P(PSTR("Base\n"), false);
+            oled_write_P(PSTR("Default\n"), false);
             break;
-        case _ONE:
-            oled_write_P(PSTR("One\n"), false);
+        case _RAISE:
+            oled_write_P(PSTR("Colemak\n"), false);
             break;
-        case _FN:
+        case _ADJUST:
             oled_write_P(PSTR("Function\n"), false);
             break;
-        case _ADJ:
-            oled_write_P(PSTR("Adjust\n"), false);
+        case _ONE:
+            oled_write_P(PSTR("Layer\n"), false);
             break;
         default:
             oled_write_P(PSTR("Undefined\n"), false);
@@ -99,7 +105,38 @@ void oled_task_user(void) {
     oled_write_P(led_state.caps_lock ? PSTR("Capslock On \n") : PSTR("           \n"), false);
 }
 #endif
-
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case _ONE:
+      if (record->event.pressed) {
+        layer_on(_ONE);
+        update_tri_layer(_ONE, _RAISE, _ADJUST);
+      } else {
+        layer_off(_ONE);
+        update_tri_layer(_ONE, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case _RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_ONE, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_ONE, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case _ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
 bool encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) {         // First encoder - right
     if (clockwise) {
