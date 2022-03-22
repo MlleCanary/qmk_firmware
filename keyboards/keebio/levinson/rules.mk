47310cc790c204036f373eb508a3d0e791328ed0
MlleCanary <soccergirl2354@gmail.com>
2022-03-22 02:01:41 -0400

    Merge branch 'master' of https://github.com/MlleCanary/qmk_firmware

--- keyboards/keebio/levinson/rules.mk
+++ keyboards/keebio/levinson/rules.mk
@@ -15,7 +15,7 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
 
 SPLIT_KEYBOARD = yes
 
